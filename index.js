bd1459eb31c6515c4e7caad9bded162705430fe9
Brian Wong <brianw385@gmail.com>
2020-03-01 02:18:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,17 +10,19 @@ return array;
 }
 
 function johnLennonFacts(facts) {
-  var array2 = [];
+  var array2 = []
   var facts = ["He was the last Beatle to learn to drive",
   "He was never a vegetarian",
   "He was a choir boy and boy scout",
-  "He hated the sound of his own voice"];
+  "He hated the sound of his own voice",
+  "While on tour, he enjoyed playing Monopoly",
+  "Harmonica was the first instrument Lennon learned to play"];
   var i = 0;
   while (i < facts.length) {
   facts.push(facts[i] + "!!!");
   i++
 }
-return facts;
+return array2;
 }
 
 function iLoveTheBeatles(number) {

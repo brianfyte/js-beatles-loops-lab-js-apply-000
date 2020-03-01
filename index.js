1e9772679252a7b6ba28b0990926028c264aa2ac
Brian Wong <brianw385@gmail.com>
2020-03-01 02:21:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function theBeatlesPlay(musicians, instruments) {
 return array;
 }
 
-function johnLennonFacts() {
+function johnLennonFacts(facts) {
   var array2 = []
   var facts = ["He was the last Beatle to learn to drive",
   "He was never a vegetarian",

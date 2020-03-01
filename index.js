a654f4081c062123d48968fc53c8787a37b4ae79
Brian Wong <brianw385@gmail.com>
2020-03-01 02:20:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function johnLennonFacts() {
   "Harmonica was the first instrument Lennon learned to play"];
   var i = 0;
   while (i < facts.length) {
-  facts.push(facts[i] + "!!!");
+  array2.push(facts[i] + "!!!");
   i++
 }
 return array2;
